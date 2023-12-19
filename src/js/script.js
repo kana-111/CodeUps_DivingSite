7d3fbc01a94551f8899d6d1af3bd2d343c73d248
Kana Hiramatsu <k_hiramatsu@cent-onze.com>
2023-12-20 01:40:48 +0900

    1219_修正

--- src/js/script.js
+++ src/js/script.js
@@ -73,7 +73,7 @@ const campaign__slider = new Swiper(".js-campaign-slider", {
   breakpoints: {
     767: {
       spaceBetween: 40,
-    }
+    },
   },
   navigation: {
     nextEl: ".campaign__slider-next",
@@ -158,4 +158,41 @@ $(".page-top").click(function () {
     400
   ); //ページトップスクロールの速さ。数字が大きいほど遅くなる
   return false; //リンク自体の無効化
+});
+
+//tab切り替え
+//任意のタブにURLからリンクするための設定
+function GethashID(hashIDName) {
+  if (hashIDName) {
+    $(".tab__item")
+      .find("a")
+      .each(function () {
+        //タブ内のaタグ全てを取得
+        var idName = $(this).attr("href"); //タブ内のaタグのリンク名（例）#lunchの値を取得
+        if (idName == hashIDName) {
+          //リンク元の指定されたURLのハッシュタグ（例）http://example.com/#lunch←この#の値とタブ内のリンク名（例）#lunchが同じかをチェック
+          var parentElm = $(this).parent(); //タブ内のaタグの親要素（li）を取得
+          $(".tab__item").removeClass("active"); //タブ内のliについているactiveクラスを取り除き
+          $(parentElm).addClass("active"); //リンク元の指定されたURLのハッシュタグとタブ内のリンク名が同じであれば、liにactiveクラスを追加
+          //表示させるエリア設定
+          $(".tab__content-area").removeClass("is-active"); //もともとついているis-activeクラスを取り除き
+          $(hashIDName).addClass("is-active"); //表示させたいエリアのタブリンク名をクリックしたら、表示エリアにis-activeクラスを追加
+        }
+      });
+  }
+}
+
+//タブをクリックしたら
+$(".tab__item a").on("click", function () {
+  var idName = $(this).attr("href"); //タブ内のリンク名を取得
+  GethashID(idName); //設定したタブの読み込みと
+  return false; //aタグを無効にする
+});
+
+// 上記の動きをページが読み込まれたらすぐに動かす
+$(window).on("load", function () {
+  $(".tab__item:first-of-type").addClass("active"); //最初のliにactiveクラスを追加
+  $(".tab__content-area:first-of-type").addClass("is-active"); //最初の.areaにis-activeクラスを追加
+  var hashName = location.hash; //リンク元の指定されたURLのハッシュタグを取得
+  GethashID(hashName); //設定したタブの読み込み
 });
